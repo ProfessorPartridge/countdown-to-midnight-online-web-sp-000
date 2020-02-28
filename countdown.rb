61e6d0ab64b43f63b24f71e534d0eaa80edfe580
Josiah  Partridge <jpartridj94@gmail.com>
2020-02-28 23:03:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
 def countdown(int)
   while int != 0
-    puts "#{int} SECOND(S)!"
+    puts "#{int} SECOND(S)!\n"
     int -= 1
   end
   "HAPPY NEW YEAR!"
@@ -8,7 +8,7 @@ end
 
 def countdown_with_sleep(int)
   while int != 0
-    puts "#{int} SECOND(S)!"
+    puts "#{int} SECOND(S)!\n"
     sleep(1)
     int -= 1
   end
